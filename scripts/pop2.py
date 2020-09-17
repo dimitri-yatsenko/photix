61ba3ca46a6f3da36d13040fde7e0cb7a07e02e9
dimitri.yatsenko@gmail.com <ubuntu@ip-172-31-89-207.ec2.internal>
2020-09-17 10:28:39 +0000

    add sim.Fluorescence to populate scripts

--- scripts/pop2.py
+++ scripts/pop2.py
@@ -2,3 +2,4 @@ from photix import sim
 
 sim.Tissue.populate(display_progress=True, reserve_jobs=True)
 sim.Fluorescence.populate(display_progress=True, reserve_jobs=True)
+sim.Detection.populate(display_progress=True, reserve_jobs=True)
